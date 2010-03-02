f73261a3c29f4c0a17528743e001aa377acee8d2
John MacFarlane <jgm@berkeley.edu>
2010-03-01 22:59:14 -0800

    Proper option parsing etc. for illuminate.

--- illuminate.hs
+++ illuminate.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE CPP #-}
 import Text.Highlighting.Illuminate
 import System.Environment
-import qualified Text.Html as Html
-import qualified Text.XHtml as XHtml
+import qualified Text.Html as H
+import qualified Text.XHtml as X
 import Control.Monad
 -- Note: ghc >= 6.12 (base >=4.2) supports unicode through iconv
 -- So we use System.IO.UTF8 only if we have an earlier version
@@ -10,67 +10,153 @@ import Control.Monad
 import Prelude
 import System.IO (hPutStrLn)
 #else
-import Prelude hiding (putStr, putStrLn, readFile)
+import Prelude hiding (putStr, putStrLn, readFile, getContents, writeFile)
 import System.IO.UTF8
 #endif
 import System.IO (stderr)
 import System.Exit
-import Data.List (intercalate, find, isPrefixOf)
+import Data.List (intercalate)
+import Data.Char (toLower)
+import System.Console.GetOpt
+
+data Flag = Help
+          | Fragment
+          | Version
+          | List
+          | NumberLines
+          | StartNum String
+          | Outfile String
+          | Syntax String
+          | Style String
+          | Format String 
+          deriving (Eq, Show)
+
+cmdOpts :: [OptDescr Flag]
+cmdOpts =
+  [ Option ['h'] ["help"] (NoArg Help)   "show usage message"
+  , Option ['v'] ["version"] (NoArg Version)   "print version"
+  , Option ['f'] ["fragment"] (NoArg Fragment)  "fragment, without document header"
+  , Option ['l'] ["list"] (NoArg List)   "list available language syntaxes"
+  , Option ['n'] ["number"] (NoArg NumberLines)  "number lines"
+  , Option []    ["start"] (ReqArg StartNum "NUMBER")  "start numbering at"
+  , Option ['o'] ["outfile"] (ReqArg Outfile "FILE")  "name of output file"
+  , Option []    ["syntax"] (ReqArg Syntax "SYNTAX")  "specify language syntax to use"
+  , Option ['s'] ["style"] (ReqArg Style "STYLE")  "specify highlighting style"
+  , Option ['t'] ["to"] (ReqArg Format "FORMAT")  "to format [ansi|latex|html|xhtml|htmlcss|xhtmlcss]"
+  ]
+
+getSyntax :: [Flag] -> Maybe String
+getSyntax [] = Nothing
+getSyntax (Syntax s : _) = Just s
+getSyntax (_:xs) = getSyntax xs
+
+getStartNum :: [Flag] -> Int
+getStartNum [] = 1
+getStartNum (StartNum n : _) = read n
+getStartNum (_:xs) = getStartNum xs
+
+getOutfile :: [Flag] -> String
+getOutfile [] = "-" 
+getOutfile (Outfile f : _) = f
+getOutfile (_:xs) = getOutfile xs
+
+getFormat :: [Flag] -> String
+getFormat [] = "ansi"
+getFormat (Format f : _) = map toLower f
+getFormat (_:xs) = getFormat xs
+
+getStyle :: [Flag] -> String
+getStyle [] = "colorful" 
+getStyle (Style s : _) = map toLower s
+getStyle (_:xs) = getStyle xs
+
+toUnixLineEndings :: String -> String
+toUnixLineEndings = filter (/='\r')
 
 main :: IO ()
 main = do
-  args <- getArgs
-  let isOpt ('-':_) = True
-      isOpt _       = False
-  let opts = filter isOpt args
-  let fnames = filter (not . isOpt) args
-  let showLexer l = putStrLn $ name l ++ " (" ++
-        intercalate ", " (aliases l) ++ "): " ++
-        intercalate ", " (filenames l) 
-  when ("-list" `elem` opts) $ mapM_ showLexer lexers >> exitWith ExitSuccess
+  (opts, fnames, errs) <- getArgs >>= return . getOpt Permute cmdOpts
+  prg <- getProgName
+  let usageHeader = prg ++ " [options] [files...]"
   
-  when (null fnames) $ usageAndExit
+  when (not . null $ errs) $
+    ioError (userError $ concat errs ++ usageInfo usageHeader cmdOpts)
 
-  let file = head fnames
-  s <- readFile file
-  let tokens = case tokenize (lexerByFilename file) s of
+  let showLexer l = putStrLn $ name l ++ " (" ++
+                               intercalate ", " (aliases l) ++ "): " ++
+                               intercalate ", " (filenames l) 
+  when (List `elem` opts) $
+     mapM_ showLexer lexers >> exitWith ExitSuccess
+
+  when (Help `elem` opts) $
+     hPutStrLn stderr (usageInfo usageHeader cmdOpts) >>
+     exitWith (ExitFailure 1)
+
+  when (Version `elem` opts) $
+     -- TODO add version using cabal
+     putStrLn (prg ++ " " ++ " - (c) 2010 John MacFarlane") >> 
+     exitWith ExitSuccess
+
+  s <- case fnames of
+          []    -> getContents
+          (x:_) -> readFile x
+
+  let lexer = case getSyntax opts of
+                  Just x  -> lexerByName x
+                  Nothing -> case fnames of
+                                []    -> Nothing
+                                (x:_) -> lexerByFilename x
+
+  let (output, fname) = case getOutfile opts of
+                              "-"   -> (putStr, "-")
+                              f     -> (writeFile f, f)
+
+  let numberlines = NumberLines `elem` opts
+  let startnum = getStartNum opts
+  let fragment = Fragment `elem` opts
+
+  style <- case getStyle opts of
+                 "colorful"   -> return colorful
+                 "monochrome" -> return monochrome
+                 x            -> hPutStrLn stderr ("Unknown style `" ++ x ++ "'") >>
+                                 exitWith (ExitFailure 3)
+
+  let options = defaultOptions { optStyle = style
+                               , optNumberLines = numberlines
+                               , optStartNumber = startnum }
+
+  let inHtml includes x = if fragment
+                             then foldr (H.renderHtml' 0) "" (H.getHtmlElements x)
+                             else H.renderHtml $ H.header H.<< includes H.+++ H.body H.<< x
+  let inXHtml includes x = if fragment
+                              then X.showHtmlFragment x
+                              else X.showHtml $ X.header X.<< includes X.+++ X.body X.<< x
+
+  let addLaTeXHeadFoot x = if fragment then x else unlines
+                           [ "\\documentclass{report}"
+                           , "\\usepackage{fancyvrb}"
+                           , "\\usepackage[usenames,dvipsnames]{color}"
+                           , "\\begin{document}"
+                           , x
+                           , "\\end{document}" ]
+
+  let tokens = case tokenize lexer $ toUnixLineEndings s of
                     Right toks  -> toks
                     Left err    -> error $ show err
-  let numberlines = "-number" `elem` opts
-  let startnum = case find ("-startnum=" `isPrefixOf`) opts of
-                      Just x -> read $ drop 10 x
-                      Nothing -> 1
-  let defOptions = defaultOptions { optNumberLines = numberlines
-                                  , optStartNumber = startnum }
-  let options = if "-mono" `elem` opts
-                   then defOptions{ optStyle = monochrome }
-                   else defOptions{ optStyle = colorful }
-  putStr $ if "-html" `elem` opts
-              then if "-css" `elem` opts
-                      then Html.renderHtml $ Html.header Html.<< [Html.thetitle Html.<< file, Html.style Html.! [Html.thetype "text/css"] Html.<< cssFor options]
-                                Html.+++ Html.body Html.<< [toHtmlCSS options tokens]
-                      else Html.renderHtml $ toHtmlInline options tokens 
-              else if "-xhtml" `elem` opts
-                   then if "-css" `elem` opts
-                        then XHtml.showHtml $ XHtml.header XHtml.<< [XHtml.thetitle XHtml.<< file, XHtml.style XHtml.! [XHtml.thetype "text/css"] XHtml.<< cssFor options]
-                                XHtml.+++ XHtml.body XHtml.<< [toXHtmlCSS options tokens]
-                        else XHtml.showHtml $ toXHtmlInline options tokens
-                   else if "-latex" `elem` opts
-                        then addLaTeXHeadFoot $ toLaTeX options tokens
-                        else toANSI options tokens
 
-addLaTeXHeadFoot :: String -> String
-addLaTeXHeadFoot s = unlines
-  [ "\\documentclass{report}"
-  , "\\usepackage{fancyvrb}"
-  , "\\usepackage[usenames,dvipsnames]{color}"
-  , "\\begin{document}"
-  , s
-  , "\\end{document}" ]
-
-usageAndExit :: IO ()
-usageAndExit = do
-  prog <- getProgName
-  hPutStrLn stderr $ "Usage:  " ++ prog ++ " [-html|-ansi|-xhtml|-latex] [-css] [-mono] [-list] [-number] [-startnum=n] file"
-  exitWith $ ExitFailure 1
+  case getFormat opts of
+         "html"       -> output $ inHtml [H.thetitle H.<< fname] $
+                         toHtmlInline options tokens
+         "xhtml"      -> output $ inXHtml [X.thetitle X.<< fname] $
+                         toXHtmlInline options tokens
+         "htmlcss"    -> output $ inHtml [ H.thetitle H.<< fname
+                                         , H.style H.! [H.thetype "text/css"] H.<< cssFor options ] $
+                         toHtmlCSS options tokens
+         "xhtmlcss"   -> output $ inXHtml [ X.thetitle X.<< fname
+                                          , X.style X.! [X.thetype "text/css"] X.<< cssFor options ] $
+                         toXHtmlCSS options tokens
+         "latex"      -> output $ addLaTeXHeadFoot $ toLaTeX options tokens
+         "ansi"       -> output $ toANSI options tokens
+         x            -> hPutStrLn stderr ("Unknown format `" ++ x ++ "'") >>
+                         exitWith (ExitFailure 5)
 
